a996a07e7f3319cd18f1e97e8f740e266457e171
André Sterba <github@andre-sterba.de>
2019-05-27 20:15:24 +0200

    Move addStreamer into internal

--- cmd/add.go
+++ cmd/add.go
@@ -21,7 +21,7 @@ streamstatus add [name] [category] -> shows all streamers in category code.`,
 	},
 
 	Run: func(cmd *cobra.Command, args []string) {
-		addNewStreamer(args)
+		internal.AddNewStreamer(args)
 	},
 }
 
@@ -34,21 +34,4 @@ func checkArgs(args []string) error {
 		return errors.New("Please provide the name and category for streamer to be added")
 	}
 	return nil
-}
-
-func addNewStreamer(args []string) {
-	streamer := args[0]
-	category := args[1]
-
-	// If the file doesn't exist create it. If it exists append to the file.
-	f, err := os.OpenFile(internal.GetUserHomeDir(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err := f.Write([]byte(streamer + " " + category + "\n")); err != nil {
-		log.Fatal(err)
-	}
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
-	}
 }
--- internal/files.go
+++ internal/files.go
@@ -46,3 +46,20 @@ func GetUserHomeDir() string {
 	}
 	return usr.HomeDir + "/.streamstatus"
 }
+
+func AddNewStreamer(args []string) {
+	streamer := args[0]
+	category := args[1]
+
+	// If the file doesn't exist create it. If it exists append to the file.
+	f, err := os.OpenFile(internal.GetUserHomeDir(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := f.Write([]byte(streamer + " " + category + "\n")); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
